8cca355c8cec56db08ae88b082a5c32a108e2cf7
ViperTools <cboatyt@gmail.com>
2023-03-06 21:50:11 -0600

    Add smake.spinner

--- library/smake.lua
+++ library/smake.lua
@@ -1,7 +1,15 @@
 ---@meta
 
+---@class spinner
+---@field symbols table A table of strings to use for the animation
+---@field interval number The delay between each frame in seconds
+---@field start fun(text: string) Starts the spinner animation with the given text
+---@field stop fun() Stops the animation
+spinner = {}
+
 ---@class smake
 ---@field config table A table representing the global and local configurations combined. This allows local configurations to override the global configuration.
+---@field spinner spinner A table holding information about the spinner
 smake = {}
 
 ---Imports a Smake plugin
