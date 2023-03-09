6f1a3225b6a43e095ef66225097746a4b7d9d8ea
ViperTools <cboatyt@gmail.com>
2023-03-09 17:06:57 -0600

    Add docs for enhancedSpinner.Call

--- library/plugins/enhancedSpinner.lua
+++ library/plugins/enhancedSpinner.lua
@@ -19,3 +19,10 @@ function Start(text) end
 ---Stops the spinner animation
 ---@param text? string Prints the text after the spinner stops if text is provided
 function Stop(text) end
+
+---Calls a function and starts and stops a spinner with the given text
+---@param func function The function to call
+---@param startText string The text to call EnhancedSpinner.Start with
+---@param endText string The text to call EnhancedSpinner.Stop with
+---@param ... any Arguments to pass to the function
+function Call(func, startText, endText, ...) end
